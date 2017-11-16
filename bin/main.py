eaa9eaa490acc9dbc53b5a70263eab80deca2c39
HoreaM <horea_manita@yahoo.com>
2017-11-16 16:13:37 +0200

    cleanup main, +mail template

--- bin/main.py
+++ bin/main.py
@@ -4,19 +4,19 @@ from spiders.cel import Cel
 from utils.utils import write_json
 
 
-class Main:
+def start_spiders():
     product_list = []
+    start_time = time.strftime("%d.%m.%Y-%H.%M")
+    # cel.ro spider
+    cel = Cel(start_time)
+    product_list.extend(cel.start_requests())
 
-    def spiders(self):
-        start_time = time.strftime("%d.%m.%Y-%H.%M")
-        cel = Cel(start_time)
-        self.product_list.extend(cel.start_requests())
-        write_json(start_time, self.product_list)
+
+    write_json(start_time, product_list)
 
 
 def main():
-    start = Main()
-    start.spiders()
+    start_spiders()
 
 
 if __name__ == '__main__':
